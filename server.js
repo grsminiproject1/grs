4993848ef4e9a86d03b04c7d44b4b109aaac0851
grsminiproject1 <grs.miniproject1@gmail.com>
2026-02-13 12:03:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const server = http.createServer(app);
 
-// 🔥 IMPORTANT FIX
 const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
-    },
-    transports: ["websocket", "polling"]
+    }
 });
 
 app.use(express.static("public"));
@@ -23,13 +21,12 @@ app.get("/create", (req, res) => {
     res.redirect(`/room/${roomId}`);
 });
 
-// Room route
+// Room page
 app.get("/room/:roomId", (req, res) => {
     res.sendFile(__dirname + "/public/room.html");
 });
 
 io.on("connection", (socket) => {
-    console.log("New user connected:", socket.id);
 
     socket.on("joinRoom", (roomId) => {
         socket.join(roomId);
@@ -37,15 +34,18 @@ io.on("connection", (socket) => {
         console.log("User joined room:", roomId);
     });
 
+    socket.on("move", (data) => {
+        socket.to(data.roomId).emit("move", data.move);
+    });
+
     socket.on("chatMessage", (msg) => {
-        if (socket.roomId) {
-            io.to(socket.roomId).emit("chatMessage", msg);
-        }
+        io.to(socket.roomId).emit("chatMessage", msg);
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+        console.log("User disconnected");
     });
+
 });
 
 const PORT = process.env.PORT || 3000;
