2dafb1a5a6bc4b566b8e7915af41918794b95146
grsminiproject1 <grs.miniproject1@gmail.com>
2026-02-13 17:20:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { Chess } = require("chess.js");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,42 +8,27 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-const games = {}; // roomId -> Chess instance
+io.on("connection", socket => {
 
-io.on("connection", (socket) => {
-
-    socket.on("joinRoom", (roomId) => {
+    socket.on("joinRoom", roomId => {
         socket.join(roomId);
         socket.roomId = roomId;
-
-        if (!games[roomId]) {
-            games[roomId] = new Chess();
-        }
-
-        socket.emit("gameState", games[roomId].fen());
         socket.to(roomId).emit("system", "Opponent joined");
     });
 
-    socket.on("move", ({ from, to }) => {
-        const game = games[socket.roomId];
-        if (!game) return;
-
-        const move = game.move({ from, to, promotion: "q" });
-        if (!move) return; // illegal move ignored
-
-        io.to(socket.roomId).emit("gameState", game.fen());
+    socket.on("move", data => {
+        socket.to(socket.roomId).emit("move", data);
     });
 
-    socket.on("chatMessage", (msg) => {
-        if (!socket.roomId || !msg.trim()) return;
-        io.to(socket.roomId).emit("chatMessage", msg.trim());
+    socket.on("chat", msg => {
+        if (!msg || !socket.roomId) return;
+        io.to(socket.roomId).emit("chat", msg);
     });
 
     socket.on("disconnect", () => {
-        if (socket.roomId) {
-            socket.to(socket.roomId).emit("system", "Opponent disconnected");
-        }
+        if (socket.roomId)
+            socket.to(socket.roomId).emit("system", "Opponent left");
     });
 });
 
-server.listen(3000, () => console.log("Server running"));
+server.listen(3000, () => console.log("Server running on 3000"));
